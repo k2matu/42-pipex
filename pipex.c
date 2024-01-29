b7d27d3a4478746f617a2c75747bc51feba00911
Kim sang Matjuhin <kmatjuhi@c1r5p21.hive.fi>
2024-01-29 14:00:33 +0200

    mess

--- pipex.c
+++ pipex.c
@@ -6,59 +6,112 @@
 /*   By: kmatjuhi <kmatjuhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:06:33 by kmatjuhi          #+#    #+#             */
-/*   Updated: 2024/01/26 11:12:50 by kmatjuhi         ###   ########.fr       */
+/*   Updated: 2024/01/29 14:00:21 by kmatjuhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	xcute_cmd1(int file1, int file2, char *cmd, int *fd)
+// void duplicate(int file1, int *fd)
+// {
+// 	dup2(file1, STDIN_FILENO);
+// 	dup2(file2, STDOUT_FILENO);
+// 	close(file1);
+// 	close(file2);
+// 	close(fd[0]);
+// }
+
+char	**parsing_PATH(char **envp, char *str)
 {
-	char **args;
-	char **arr;
-	char *path;
-	
-	dup2(file1, STDIN_FILENO);
-	dup2(file2, STDOUT_FILENO);
-	close(file1);
-	close(file2);
-	close(fd[0]);
-	args = ft_split(cmd, ' ');
-	path = ft_strjoin("/bin/", args[0]);
-	execve(path, args, NULL);
-	perror("execve");
+	char	**path_to_env;
+	char	*path;
+	int		i;
+
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			path = ft_substr(envp[i], 5, ft_strlen(envp[i]));
+		i++;
+	}
+	path_to_env = ft_split(path, ':');
+	i = 0;
+	while (path[i])
+	{
+		path_to_env[i] = ft_strjoin(path_to_env[i], "/");
+		path_to_env[i] = ft_strjoin(path_to_env[i], str);
+		printf("%s", path_to_env[i]);
+		i++;
+	}
+	return (path_to_env);
 }
 
-void pipex(char *argv[], int file1, int file2)
+void	xcute_cmd1(char *cmd, char **envp)
 {
-	int fd[2];
-	int	pid1;
-	int buffer;
+	char	**args;
+	char	**path;
+	int		i;
+
+	i = 0;
+	args = ft_split(cmd, ' ');
+	printf("executing 1\n");
+	path = parsing_PATH(envp, args[0]);
+	while (path[i])
+	{
+		printf("%s", path[i]);
+		// execve(path[i], args, envp);
+		// perror("execve");
+		// free(path[i]);
+		i++;
+	}
+}
+
+void	pipex(char **argv, int file1, char **envp)
+{
+	int	fd[2];
+	pid_t	pid;
 
 	if (pipe(fd) == -1)
 		return (perror("Pipe: "));
-	pid1 = fork();
-	if (pid1 < 0)
-		return (perror("Fork: "));
-	if (pid1 == 0)
-		xcute_cmd1(file1, file2, argv[2], fd);
+
+	// if (pid == 0)
+	// {
+	// 	xcute_cmd1(argv[2], envp);
+	// }
+	// else
+	// 	printf("hello");
+
+	//     pid_t pid;
+
+    printf("Fork here.\n");
+    pid = fork();
+    if (pid == -1)
+        return (perror("Fork: "));
+    if (pid == 0)
+    {
+        // Fork's return value is 0:
+        // we are now in the child process
+        printf("Child: I'm the child, my internal pid is %d.\n", pid);
+		xcute_cmd1(argv[2], envp);
+    }
+    else if (pid > 0)
+    {
+        // Fork's return value is not 0
+        // which means we are in the parent process
+        printf("Parent: I'm the parent, my child's pid is %d.\n", pid);
+	}
 }
 
-int	main(int argc, char *argv[])
+
+int	main(int argc, char *argv[], char *envp[])
 {
 	int	file1;
 	int	file2;
-	
+
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 		return (-1);
-	file2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC);
-	if (file2 == -1)
-	{
-		close(file1);
-		return (-1);
-	}
-	pipex(argv, file1, file2);
+	pipex(argv, file1, envp);
 	return (0);
 }
 
